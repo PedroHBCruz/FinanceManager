686349c5380224b0fdc7ec5a1c639aacf6b9da9b
PedroHBCruz <pedro.henriqueIFMT@hotmail.con>
2021-08-13 13:43:46 -0400

    Implementando os métodos de salvar, atualizar, deletar, atualizarStatus e buscar lançamentos

--- src/main/java/br/com/FinanceManager/services/LancamentoServiceImpl.java
+++ src/main/java/br/com/FinanceManager/services/LancamentoServiceImpl.java
@@ -1,7 +1,13 @@
 package br.com.FinanceManager.services;
 
 import java.util.List;
+import java.util.Objects;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,32 +22,41 @@ public class LancamentoServiceImpl implements LancamentoService{
 	private LancamentoRepository repository;
 	
 	@Override
+	@Transactional
 	public Lancamento salvar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(lancamento);
 	}
 
 	@Override
+	@Transactional
 	public Lancamento atualizar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		return null;
+		Objects.requireNonNull(lancamento.getId());
+		return repository.save(lancamento);
 	}
 
 	@Override
+	@Transactional
 	public void deletar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
+		Objects.requireNonNull(lancamento.getId());
+		repository.delete(lancamento);
 		
 	}
 
 	@Override
-	public List<Lancamento> buscar(Lancamento LancamentoFiltro) {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		Example example = Example.of( lancamentoFiltro, 
+				ExampleMatcher.matching()
+					.withIgnoreCase()
+					.withStringMatcher(StringMatcher.CONTAINING) );
+		
+		return repository.findAll(example);
 	}
-
+ 
 	@Override
 	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
-		// TODO Auto-generated method stub
+		lancamento.setStatus(status);
+		atualizar(lancamento);
 		
 	}
 
